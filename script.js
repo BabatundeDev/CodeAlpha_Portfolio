6a1b59cee81c258f8bb93554374d2e8a3e893d03
Ishau Babatunde <ishautunde700@gmail.com>
2025-04-22 11:42:05 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ scrollToTopBtn.addEventListener("click", function () {
 
 document.addEventListener("DOMContentLoaded", () => {
   const scrollRevealOptions = {
-    distance: "25px",
+    distance: "20px",
     duration: 1200,
     easing: "cubic-bezier(0.5, 0, 0, 1)", 
     opacity: 0,
